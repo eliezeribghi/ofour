8540443eef8e55fd5314afdb86c2ca6928da28ce
eliezer ibghi <iibgui@yahoo.fr>
2025-01-03 11:19:01 +0100

    update dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Remplacer la variable ${PORT} par sa valeur dans le modèle de configuration
-envsubst '\$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '\$PORT' < ./backend/nginx.conf > /etc/nginx/nginx.conf
 
 # Lancer le service nginx et PHP-FPM
 exec "$@"
