e2154656d8e1a9bf2158418c94721416e3380c74
eliezer ibghi <iibgui@yahoo.fr>
2025-01-03 11:45:19 +0100

    update dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+echo "Listing files in the / directory:"
+ls -la /
+
+# Vérifier le répertoire /backend
 echo "Listing files in the /backend directory:"
-ls -la ./backend
+ls -la /backend
 
 # Remplacer la variable ${PORT} par sa valeur dans le modèle de configuration
-envsubst '\$PORT' < ./backend/nginx.conf > /etc/nginx/nginx.conf
+envsubst '\$PORT' < /backend/nginx.conf > /etc/nginx/nginx.conf
 
 # Lancer le service nginx et PHP-FPM
 exec "$@"
