d1eff91a7079f46a78920946b9ea0cf0b2d9ee93
eliezer ibghi <iibgui@yahoo.fr>
2025-01-03 11:32:00 +0100

    update dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "Listing files in the /backend directory:"
+ls -la ./backend
+
 # Remplacer la variable ${PORT} par sa valeur dans le modèle de configuration
 envsubst '\$PORT' < ./backend/nginx.conf > /etc/nginx/nginx.conf
 
