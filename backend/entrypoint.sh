df1982176cb0313c219d6ffb3d706cbbba102940
eliezer ibghi <iibgui@yahoo.fr>
2025-01-06 16:16:58 +0100

    update dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# Vérifier si le répertoire /backend existe
-if [ ! -d "/backend" ]; then
-  echo "Le répertoire /backend n'existe pas."
-  exit 1
-fi
+# Démarrer PHP-FPM
+service php8.2-fpm start
 
-# Remplacer la variable ${PORT} par sa valeur dans le modèle de configuration
-envsubst '\$PORT' < /backend/nginx.conf > /etc/nginx/nginx.conf
+# Démarrer Nginx
+service nginx start
 
-# Lancer le service nginx et PHP-FPM
-exec "$@"
+# Maintenir le conteneur actif
+tail -f /dev/null
