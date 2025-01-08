d46ded24898178a220b6b2f409e81c1457add72e
eliezer ibghi <iibgui@yahoo.fr>
2025-01-08 22:39:11 +0100

    tes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // Configuration Vite
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 3000,
-  },
+ 
   plugins: [svelte()],
   define: {
 
