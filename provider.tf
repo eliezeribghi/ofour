f667df35b6afc3edd765d9a7095cfa80f41a2e76
eliezer ibghi <iibgui@yahoo.fr>
2025-02-18 21:06:07 +0100

    modification des sonar-project-front

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+user="iibghi"
+fingerprint="071c2d4ff669824d53cbc714510bdb64e8488c99c9e2cafd5cbf89058a246539"
+tenancy="ocid1.tenancy.oc1..aaaaaaaasyobvmmuox4y5l4v47ubn367uglbpk3y7xgu6orf7p5eeflb4dgq"
+region="eu-frankfurt-1"
+
+}
