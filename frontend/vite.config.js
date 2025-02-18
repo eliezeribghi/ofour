02e7cbec8f109813ccbd71ab054f6682d2552c68
eliezer ibghi <iibgui@yahoo.fr>
2025-02-18 20:30:25 +0100

    integration SonarQube et le fichier .yml pour exécuter SonarQube automatiquement à chaque push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  
+  root: "./", // spécifie que la racine du projet est à la racine du dossier frontend
+  build: {
+    outDir: '../dist', // le dossier de sortie après le build
+  },
   css: {
     preprocessorOptions: {
       sass: {
